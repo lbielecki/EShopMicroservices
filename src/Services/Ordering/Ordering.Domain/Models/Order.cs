3f426bccbd40769da9d0e346d50ab97b0d64afbc
lbielecki <lucjan.bielecki@sanokrubber.pl>
2024-12-09 15:08:26 +0100

    c.d.

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,38 @@
+namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<OrderId>
+{
+    private readonly List<OrderItem> _orderItems = new();
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+    
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress,
+        Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+        
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+        return order;
+    }
+}
