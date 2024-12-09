554c1676db4898148bffbce31a595a3567bd6c69
lbielecki <lucjan.bielecki@sanokrubber.pl>
2024-12-09 18:45:24 +0100

    c.d.

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -35,4 +35,33 @@ public class Order : Aggregate<OrderId>
         order.AddDomainEvent(new OrderCreatedEvent(order));
         return order;
     }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = status;
+        
+        AddDomainEvent(new OrderUpdateEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+        if (orderItem != null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+    }
 }
