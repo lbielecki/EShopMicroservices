84f6b5434c92b69640409bdf9e9c3aaebf04348f
lbielecki <lucjan.bielecki@sanokrubber.pl>
2024-12-13 09:07:08 +0100

    Order c.d.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).HasConversion(
+            productId => productId.Value,
+            dbId => ProductId.Of(dbId));
+        
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+    }
+}
